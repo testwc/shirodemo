6096b92fa28c1de756b8c72b7b21d53d55fdd6da
wang.chuan <wgcn310@qq.com>
2021-01-25 17:54:18 +0800

    first commit

---
+++ src/main/java/com/example/shirodemo/ShiroConfig.java
@@ -0,0 +1,28 @@
+package com.example.shirodemo;
+
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+    @Bean
+    MyRealm myRealm() {
+        return new MyRealm();
+    }
+    @Bean
+    DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        manager.setRealm(myRealm());
+        return manager;
+    }
+    @Bean
+    ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition definition = new DefaultShiroFilterChainDefinition();
+        definition.addPathDefinition("/doLogin", "anon");
+        definition.addPathDefinition("/hello/*", "authc");
+        return definition;
+    }
+}
